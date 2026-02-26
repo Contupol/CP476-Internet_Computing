1b72384a97f69e77f6bf534b656ba7861d4327b3
Carlos <alco4230@mylaurier.ca>
2026-02-26 15:30:59 -0500

    First Page login/signup forms

---
+++ script.js
@@ -0,0 +1,9 @@
+function loginDisplay() {
+    document.getElementById('signup-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
+
+function SignupDisplay() {
+    document.getElementById('signup-form').style.display = 'block';
+    document.getElementById('login-form').style.display = 'none';
+}
