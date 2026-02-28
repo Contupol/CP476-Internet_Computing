58b89cca80527b337dccdc77987032eea50add67
jnliu6 <jenniferliu545@gmail.com>
2026-02-27 23:31:01 -0500

    Update index.php
    routers

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<!-- This is the entry point.  -->
-
 <?php
+//Entry point
+$page = $_GET['page'] ?? 'first';
 
-echo "Welcome to your budget tracker!";
-$controller = new PageController();
+switch($page) {
+    case 'first':
+        include 'firstPage.html';
+        break;
+    case 'second':
+        include 'secondPage.html';
+        break;
+    case 'settings':
+        include 'settings_page.html';
+        break;
+    default:
+        echo "404 - Page not found";
+}
+?>
