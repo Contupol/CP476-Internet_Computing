56538ebd4cc0b23749994216dd99026ee6791eaf
jnliu6 <jenniferliu545@gmail.com>
2026-02-27 22:54:01 -0500

    Add files via upload
    initial index/page controller php

---
+++ index.php
@@ -0,0 +1,5 @@
+<!-- This is the entry point.  -->
+
+<?php
+
+echo "Welcome to your burdget tracker!";
---
+++ pageController.php
@@ -0,0 +1,16 @@
+<?php
+
+class PageController {
+    
+    public function getFirst() {
+        echo "first page endpoint stub: login/signup";
+    }
+
+    public function getSecond() {
+        echo "second page endpoint stub: main budgetexpenses display";
+    }
+    
+    public function getThird() {
+        echo "third page endpoint stub: settings/expense summary";
+    }
+}
